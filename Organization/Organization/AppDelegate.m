d92167da3955556366bde82598c32e2ed2e9d9e3
secwow <dima.rebenko@gmail.com>
2017-08-08 18:29:56 +0300

    Code refactor

--- Organization/Organization/AppDelegate.m
+++ Organization/Organization/AppDelegate.m
@@ -1,13 +1,6 @@
-//
-//  AppDelegate.m
-//  Organization
-//
-//  Created by New user on 01.08.17.
-//  Copyright © 2017 New user. All rights reserved.
-//
-
 #import "AppDelegate.h"
 #import "MainViewController.h"
+
 @interface AppDelegate ()
 
 @end
